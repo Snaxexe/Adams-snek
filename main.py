2c59858cf66f3df0278aa0bd6995c3ec0a1e55e3
Snaxexe <91731852+Snaxexe@users.noreply.github.com>
2023-10-27 10:09:46 +0000

    Hallo

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+
+screen_width = 800
+screen_height = 600
+
+screeen = pygame.display.set_mode((screen_width, screen_height))
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type ==pygame.QUIT:
+            run = False
+
+
+pygame.quit()
+
